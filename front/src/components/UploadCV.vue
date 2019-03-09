eaa5a7af5bcc0944155c633b568378e26b66b2aa
Mircea Sorin Sebastian <mircea.sorin.sebastian@gmail.com>
2019-03-09 17:27:06 +0200

    File Uploading Works

--- front/src/components/UploadCV.vue
+++ front/src/components/UploadCV.vue
@@ -1,8 +1,16 @@
 <template>
   <v-container>
     <v-card>
-      <v-btn style="width: 98%;" color="success" @click="$refs.inputUpload.click()">Upload CV</v-btn>
-      <input v-show="false" ref="inputUpload" type="file" @change="uploadCV">
+      <v-btn
+        type="file"
+        id="file"
+        ref="file"
+        style="width: 98%;"
+        color="success"
+        v-on:change="uploadCV()"
+        @click="$refs.file.click()"
+      >Upload CV</v-btn>
+      <input v-show="false" ref="file" type="file" @change="uploadCV">
     </v-card>
   </v-container>
 </template>
@@ -10,47 +18,31 @@
 <script>
 export default {
   data: () => ({
-    card_text: "Hello World"
+    card_text: "Hello World",
+    file: ""
   }),
   methods: {
-    uploadCV(e) {
-      const files = e.target.files;
-      if (files[0] !== undefined) {
-        const cvName = files[0].name;
-        console.log(cvName);
+    uploadCV() {
+      this.file = this.$refs.file.files[0];
 
-        if (cvName.lastIndexOf(".") <= 0) {
-          return;
-        }
+      // Create formData and append the file
+      let formData = new FormData();
+      formData.append("file", this.file);
+      console.log(formData.getAll("file"));
 
-        const fr = new FileReader();
-        fr.readAsDataURL(files[0]);
-        fr.addEventListener("load", () => {
-          var cvFile = fr.result; // this is the file that can be sent to server...
-
-          // Create formData and append the file
-          let formData = new FormData();
-          formData.append("file", cvFile);
-          console.log(formData.getAll('file'))
-
-
-          // Send the request
-          this.axios
-            .post("http://127.0.0.1:5000/submit", formData, {
-              headers: {
-                "Content-Type": "multipart/form-data"
-              }
-            })
-            .then(function() {
-              console.log("SUCCESS!!");
-            })
-            .catch(function(err) {
-              console.log("FAILURE!! ", err);
-            });
+      // Send the request
+      this.axios
+        .post("http://127.0.0.1:5000/submit", formData, {
+          headers: {
+            "Content-Type": "multipart/form-data"
+          }
+        })
+        .then(function() {
+          console.log("SUCCESS!!");
+        })
+        .catch(function(err) {
+          console.log("FAILURE!! ", err);
         });
-      } else {
-        console.log("Error when receiving the CV");
-      }
     }
   }
 };
