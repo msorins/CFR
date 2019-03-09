d64f403aae9ba77195bcd294c5ae414beaa0964d
Unknown <razvan_roatis@yahoo.com>
2019-03-09 14:21:50 +0200

    Changed gitignore

--- Evaluators/BasicEvaluator.py
+++ Evaluators/BasicEvaluator.py
@@ -1,5 +1,7 @@
+import PyPDF2
+
 class BasicEvaluator:
-    def evaluate(self, pyPDF):
+    def evaluate(self, pyPDF: PyPDF2):
         """
         :param pyPDF:
         :return: a list of PartialFeedback
