65327e1fe2d346aa61e1de9905c81d73b9512b00
Mircea Sorin Sebastian <mircea.sorin.sebastian@gmail.com>
2019-03-09 13:44:37 +0200

    Basic Structure

---
+++ Evaluators/BasicEvaluator.py
@@ -0,0 +1,7 @@
+class BasicEvaluator:
+    def evaluate(self, pyPDF):
+        """
+        :param pyPDF:
+        :return: a list of PartialFeedback
+        """
+        pass
---
+++ Models/PartialFeedback.py
@@ -0,0 +1,6 @@
+class PartialFeedback:
+    def __init__(self, message, points) -> None:
+        super().__init__()
+
+        self.message = message
+        self.points = points
