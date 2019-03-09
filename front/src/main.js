237e5719b4d864af1a6888fdd95120768239c9d8
Mircea Sorin Sebastian <mircea.sorin.sebastian@gmail.com>
2019-03-09 15:50:16 +0200

    Added front & Moved back

---
+++ front/src/main.js
@@ -0,0 +1,11 @@
+import Vue from 'vue'
+import './plugins/vuetify'
+import App from './App.vue'
+import Vuetify from 'vuetify/dist/vuetify.min.css'
+
+Vue.config.productionTip = false
+Vue.use(Vuetify)
+
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
